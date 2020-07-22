2cb34c9c04ecca8b2c0d122c642ba9c9a73dc765
PeterDeWeirdt <petedeweirdt@gmail.com>
2020-07-22 10:13:14 -0400

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gpp-rnd/gpplot',
-    version='0.3.4',
+    version='0.3.5',
     zip_safe=False,
 )
